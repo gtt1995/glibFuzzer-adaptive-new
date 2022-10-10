e26a2d8081c68d5b341e183f67ef3ede12f3674d
gtt1995 <gutaotao1995@qq.com>
2022-10-10 19:49:53 +0800

    173-first commit about human-in-the-loop

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+LIBFUZZER_SRC_DIR=$(dirname $0)
+CXX="${CXX:-clang}"
+for f in $LIBFUZZER_SRC_DIR/*.cpp; do
+#  clang++ -g -O2 -fno-omit-frame-pointer -std=c++11 $f -c &
+#  $CXX -g -O2 -fno-omit-frame-pointer -std=c++11 $f -c &
+  $CXX -g -O2 -fno-omit-frame-pointer -std=c++17 $f -c &
+done
+wait
+rm -f libFuzzer.a
+ar ru libFuzzer.a Fuzzer*.o
+rm -f Fuzzer*.o
+
